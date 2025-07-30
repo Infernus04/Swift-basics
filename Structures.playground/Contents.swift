56362a6b490f62c8a7ffb8c06db92eb9b53df1be
Ishan Magarde <magardeishan@gmail.com>
2025-07-30 15:12:34 +0530

    Structures

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -8,3 +8,21 @@ let person = Person(name : "Ishan", age : 20)
 print(person.name)
 print(person.age)
 
+//Default Initializer
+struct Odometer {
+    var count : Int = 0
+}
+
+var odometer = Odometer()
+print(odometer.count)
+
+struct BankAccount {
+    var balance : Double = 26.26
+    var bankAccountNumber : Int
+}
+
+var myAccount = BankAccount(bankAccountNumber : 123456789)
+print(myAccount.balance)
+print(myAccount.bankAccountNumber)
+
+
