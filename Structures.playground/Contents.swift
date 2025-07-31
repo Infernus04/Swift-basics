ab7201e9e3d86421190bbfaee00acb66930d94e5
Ishan Magarde <magardeishan@gmail.com>
2025-07-31 13:43:34 +0530

    custom Struct

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -9,13 +9,13 @@ print(person.name)
 print(person.age)
 
 //Default Initializer
-struct Odometer {
-    var count : Int = 0
-}
-
-var odometer = Odometer()
-print(odometer.count)
-
+//struct Odometer {
+//    var count : Int = 0
+//}
+//
+//var odometer = Odometer()
+//print(odometer.count)
+//
 struct BankAccount {
     var balance : Double = 26.26
     var bankAccountNumber : Int
@@ -26,20 +26,108 @@ print(myAccount.balance)
 print(myAccount.bankAccountNumber)
 
 
-struct Temperature {
-    var celcius : Double
-     
-    init (celcius : Double) {
-        self.celcius = celcius
+//struct Temperature {
+//    var celcius : Double
+//     
+//    init (celcius : Double) {
+//        self.celcius = celcius
+//    }
+//    
+//    init(fahrenheit : Double){
+//        celcius = (fahrenheit - 32) / 1.8
+//    }
+//}
+
+//let currentTemperature = Temperature(celcius: 18.5)
+//let boiling = Temperature(fahrenheit: 212)
+//
+//print(currentTemperature.celcius)
+//print(boiling.celcius)
+
+struct Odometer {
+    var count : Int = 0
+
+    mutating func increment() {
+        count += 1
     }
     
-    init(fahrenheit : Double){
-        celcius = (fahrenheit - 32) / 1.8
+    mutating func increment(by amount : Int){
+        count += amount
+    }
+    
+    mutating func reset() {
+        count = 0
     }
 }
 
-let currentTemperature = Temperature(celcius: 18.5)
-let boiling = Temperature(fahrenheit: 212)
+var odometer = Odometer()
+odometer.increment()
+odometer.increment(by: 15)
+print(odometer.count)
 
-print(currentTemperature.celcius)
-print(boiling.celcius)
+var odometer1 = Odometer()
+odometer1.increment()
+odometer1.increment(by: 20)
+print(odometer1.count)
+
+
+//struct Temperature1{
+//    var celcius : Double
+//    
+//    var fahrenheit : Double {
+//        (celcius * 1.8) + 32
+//    }
+//}
+//
+//let cityTemperature = Temperature1(celcius: 10)
+//print(cityTemperature.fahrenheit)
+
+struct stepCounter {
+    
+    var totalSteps : Int = 50 {
+        willSet {
+            print("About to set total steps to \(newValue) steps")
+        }
+        didSet {
+            if oldValue < totalSteps {
+                print("You took \(totalSteps - oldValue) extra steps")
+            }
+        }
+    }
+    mutating func stepIncrement(){
+        totalSteps += 1
+    }
+}
+var counter = stepCounter()
+counter.stepIncrement()
+counter.totalSteps = 100
+counter.totalSteps = 120
+counter.totalSteps = 120
+
+@MainActor
+struct Temperature {
+    @MainActor static var boilingPoint : Double = 100
+    
+    static func convertedFromFahrenheit(_ temperatureInFahrenheit : Double) -> Double {
+        (((temperatureInFahrenheit - 32) * 5) / 9)
+    }
+    
+    let boilingPoint = Temperature.boilingPoint
+    let currentTemperature = Temperature.convertedFromFahrenheit(100)
+    let positiveNumber = abs(-4.14)
+}
+
+print(Temperature.boilingPoint)
+print(Temperature.convertedFromFahrenheit(100))
+print(Temperature().positiveNumber)
+
+struct Size {
+    var width : Double
+    var height : Double
+}
+
+var someSize = Size(width: 250, height: 1000)
+var anotherSize = someSize
+someSize.width = 500
+print(someSize.width)
+print(anotherSize.width)
